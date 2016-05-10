95a8770b92616c6d5bffeb964225f21ea3e63d10
Joseph Anda <josephanda@Josephs-MBP.wp.comcast.net>
2016-05-10 15:18:58 -0700

    more UX

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//  [  ]  Add story and comment adding features
+//  [  ]  Add date & name features for adding stories and comments 
+//  [  ]  Create picture background
+//  [  ]  Solidify navbar and side navigation (if any) design
+//  [  ]  Make the layout colored and semi-finalized 
