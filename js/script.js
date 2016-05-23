c605363952993890e3939735ddb816b024669e5f
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-05-23 11:44:10 -0700

    added scrollbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 //  [  ]  Add story and comment adding features
 //  [  ]  Add date & name features for adding stories and comments 
 //  [  ]  Create picture background
-//  [  ]  Solidify navbar and side navigation (if any) design
-//  [  ]  Make the layout colored and semi-finalized 
+//  [  ]  Design banner
+//  [  ]  Create functional menu items 
+//		[!!]  Create fixed position menu 
+//  [!!]  Make the layout colored and semi-finalized 
+
+var taskbar = $('#taskbar');
+var phantom = $('#phantom');
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+
+    if (scroll >= 71) {
+        taskbar.addClass("taskbar-scroll");
+        phantom.visibility = 'visible';
+        phantom.css({position: "relative"});
+    } else {
+        taskbar.removeClass("taskbar-scroll");
+        phantom.visibility = 'hidden'; 
+        phantom.css({position: "fixed"});  
+    }
+});
