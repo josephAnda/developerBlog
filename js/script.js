c5e66539db8c22b391a82be11b05fe7144d48aa8
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-05-26 11:58:25 -0700

    refactored

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-//  [  ]  Make sure feautures don't break if screen is resized.
-//  	[  ]  Make menu mobile-friendly	(via bootstrap)
+//  [!!]  Make sure feautures don't break if screen is resized.
+//  	[!!]  Make menu mobile-friendly	(via bootstrap)
 //  [  ]  Refactor with MVC paradigm  
 //  [  ]  Add story and comment adding features
 //  [  ]  Add date & name features for adding stories and comments 
@@ -10,7 +10,28 @@
 //		[!!]  Create fixed position menu 
 //  [!!]  Make the layout colored and semi-finalized 
 //  [  ]  Host different pages by either (1)  Changing HTML or (2) Actually changing page 
+var model = {
+	taskbar: $('#taskbar'),
+	phantom: $('#phantom')  // <---This is to keep the non-taskbar content from re-flowing once the taskbar locks via position: fixed
+};
 
+var view = {
+	scrollLock: function() {
+		$(window).scroll(function() {    
+		    var scroll = $(window).scrollTop();
+		    //console.log(scroll);  // <--- Useful for debugging the scroll-dependent menu-fixing 
+		    if (scroll >= 99) {
+		        model.taskbar.addClass("taskbar-scroll");
+		        model.phantom.css({position: "relative"});
+		    } else {
+		        model.taskbar.removeClass("taskbar-scroll");
+		        model.phantom.css({position: "fixed", visibility: "hidden"});  
+		    }
+		})
+	}
+};
+
+console.log(view.scrollLock);
 var taskbar = $('#taskbar');
 var phantom = $('#phantom');
 
