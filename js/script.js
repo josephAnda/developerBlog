ebf57596eba2b0c59e257b20f1200385a5bb26fe
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-06-05 09:00:41 -0700

    token update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
 //  [!!]  Make sure feautures don't break if screen is resized.
 //  	[!!]  Make menu mobile-friendly	(via bootstrap)
-//  [  ]  Refactor with MVC paradigm  
+//      [  ]  Change the threshold scroll value for different viewport sizes
+//  [!!]  Refactor with MVC paradigm  
 //  [  ]  Add story and comment adding features
 //  [  ]  Add date & name features for adding stories and comments 
 //  [  ]  Create picture background
@@ -10,39 +11,36 @@
 //		[!!]  Create fixed position menu 
 //  [!!]  Make the layout colored and semi-finalized 
 //  [  ]  Host different pages by either (1)  Changing HTML or (2) Actually changing page 
-var model = {
-	taskbar: $('#taskbar'),
-	phantom: $('#phantom')  // <---This is to keep the non-taskbar content from re-flowing once the taskbar locks via position: fixed
-};
+(function() {
+	"use strict";
 
-var view = {
-	scrollLock: function() {
-		$(window).scroll(function() {    
-		    var scroll = $(window).scrollTop();
-		    //console.log(scroll);  // <--- Useful for debugging the scroll-dependent menu-fixing 
-		    if (scroll >= 99) {
-		        model.taskbar.addClass("taskbar-scroll");
-		        model.phantom.css({position: "relative"});
-		    } else {
-		        model.taskbar.removeClass("taskbar-scroll");
-		        model.phantom.css({position: "fixed", visibility: "hidden"});  
-		    }
-		})
-	}
-};
+	var model = {
+		taskbar: $('#taskbar'),
+		phantom: $('#phantom'),  // <---This is to keep the non-taskbar content from re-flowing once the taskbar locks via position: fixed
+	};
 
-console.log(view.scrollLock);
-var taskbar = $('#taskbar');
-var phantom = $('#phantom');
+	var view = {	
+		scrollLock: function() {
+			$(window).scroll(function() {    
+			    var scroll = $(window).scrollTop();
+			    //console.log(scroll);  // <--- Useful for debugging the scroll-dependent menu-fixing 
+			    if (scroll >= 99) {
+			        model.taskbar.addClass("taskbar-scroll");
+			        model.phantom.css({position: "relative"});
+			    } else {
+			        model.taskbar.removeClass("taskbar-scroll");
+			        model.phantom.css({position: "fixed", visibility: "hidden"});  
+			    }
+			})
+		}
+	};
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    //console.log(scroll);  // <--- Useful for debugging the scroll-dependent menu-fixing 
-    if (scroll >= 99) {
-        taskbar.addClass("taskbar-scroll");
-        phantom.css({position: "relative"});
-    } else {
-        taskbar.removeClass("taskbar-scroll");
-        phantom.css({position: "fixed", visibility: "hidden"});  
-    }
-});
+	var controller = {
+		init: function() {
+			view.scrollLock();
+		}
+	};
+	
+	controller.init();
+}) 	();
+
